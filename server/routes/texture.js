e618a3a4991fe2a4707cc2f8e78729d4c1cc61e8
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2015-01-04 22:49:59 -0500

    Use real directory name

--- server/routes/texture.js
+++ server/routes/texture.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
 
 router.get('/', function (req, res, next) {
-  fs.readdir('public/images', function (err, files) {
+  var dir = path.join(__dirname, 'public', 'images');
+  fs.readdir(dir, function (err, files) {
     if (err) return next(err);
     res.send(files);
   });
