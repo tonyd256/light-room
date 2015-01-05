dd80cde5b95a53ddc471833d60b25944dc78735f
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2015-01-04 23:17:40 -0500

    Use root

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use('/public', qt.static(path.join(__dirname, 'public')));
+app.use('/', qt.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/textures', texture);
